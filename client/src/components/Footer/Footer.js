633d56ce3339b204bd4ac38c3ce32155b9516fd5
bhanujggandhi <gandhibhanuj@gmail.com>
2020-06-02 15:50:59 +0530

    footer added

---
+++ client/src/components/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <div className="footer">
+      Bhanuj Gandhi &copy; {new Date().getFullYear()} <br />
+    </div>
+  );
+}
+
+export default Footer;
