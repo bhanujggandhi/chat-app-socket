9cd1e4cb2a0f8861e90b94a042e7e6dbb463b6b1
bhanujggandhi <gandhibhanuj@gmail.com>
2020-06-02 12:15:05 +0530

    Backend

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const socketio = require("socket.io");
 const http = require("http");
 
+const { addUser, removeUser, getUser, getUsersInRoom } = require("./users");
+
 const PORT = process.env.PORT || 5000;
 
 const router = require("./router");
@@ -13,7 +15,11 @@ const io = socketio(server);
 io.on("connection", (socket) => {
   console.log("We have a new connection");
   socket.on("join", ({ name, room }, callback) => {
-    console.log(name, room);
+    const { error, user } = addUser({ id: socket.id, name, room });
+
+    if (error) return callback(error);
+
+    socket.join(user.room);
   });
 
   socket.on("disconnect", () => {
